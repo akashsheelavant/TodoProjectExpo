a8281a1f510080bb8fdeed7f55d478ad27992fc1
Akash <akashsheelavant4@gmail.com>
2023-01-12 14:23:14 +0530

    Adding a todo.

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, FlatList, SafeAreaView } from 'react-native';
+import AddTodo from './components/addTodo';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 
 export default function App() {
   const [todos, setTodos] = useState([
-    { text: 'buy coffee', key: 1 },
-    { text: 'create an app', key: 2 },
-    { text: 'play on the switch', key: 3 }
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
   ])
 
   const handleTap = (id) => {
@@ -17,12 +18,21 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text, key: Math.random().toString() },
+        ...prevTodos
+      ];
+    })
+  }
+
   return (
     <SafeAreaView>
       <View>
         <Header />
         <View style={styles.content}>
-          {/*forms*/}
+          <AddTodo submitHandler={submitHandler} />
           <View style={styles.list}>
             <FlatList
               data={todos}
